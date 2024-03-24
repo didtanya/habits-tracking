7e78c4df4386663d69606237675c18189d2a5708
Pavel Skorikov <p.skorikov@ritm.media>
2024-03-24 22:00:31 +0300

    change dockerfile

--- tracking_project/settings.py
+++ tracking_project/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tracking_project.accounts',
-    'tracking_project.app',
+    # 'tracking_project.accounts',
+    # 'tracking_project.app',
 ]
 
 MIDDLEWARE = [
